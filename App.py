44c9bcecb01781f235aef9e3342f5473a313a657
Shane Brennan <shane.brennan.ie@gmail.com>
2016-09-12 21:21:47 +0000

    Updating README

--- App.py
+++ App.py
@@ -16,23 +16,25 @@ import os
 import csv
 import logging
 import datetime
-import sqlalchemy
-from sqlalchemy import text
-from sqlalchemy import exc
 from logging.handlers import RotatingFileHandler
-from mail.SendMail import SendMail
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
 @app.route('/')
 def index():
+
+	# The logger
+	timeStamp = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+	reqIPAddr = request.remote_addr
+	app.logger.info('%s - Analytics Request GET REQUEST index.html request from %s',timeStamp, reqIPAddr)
+
 	# Render the template
 	return render_template('index.html')
 
 
 if __name__ == '__main__':
-	handler = RotatingFileHandler('application.log', maxBytes=10000, backupCount=3)
+	handler = RotatingFileHandler('/home/ubuntu/Feature_Dashboard/application.log', maxBytes=10000, backupCount=3)
 	handler.setLevel(logging.INFO)
 	app.logger.addHandler(handler)
 	app.run(host='0.0.0.0', debug=True, port=1234)
